3863f659e15421c647d86a7c0156e9f2ebd597cf
asadkarami <asad.karammi@gmail.com>
2024-10-10 12:56:19 +0330

    chore: Added test class for HomeViewModel

---
+++ app/src/test/java/com/asad/noteapp/features/home/presentation/viewModel/HomeViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.asad.noteapp.features.home.presentation.viewModel
+
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class HomeViewModelTest {
+
+
+    @Before
+    fun setup(){
+
+    }
+
+    @After
+    fun tearDown(){
+
+    }
+
+    @Test
+    fun checkShowingIsLoadingState() = runTest {
+
+    }
+}
