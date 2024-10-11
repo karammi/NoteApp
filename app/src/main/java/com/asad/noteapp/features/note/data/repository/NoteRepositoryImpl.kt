6a6fea664d6f5163b90c0b596df4251cb5e52f14
asadkarami <asad.karammi@gmail.com>
2024-10-11 09:54:12 +0330

    chore: Refactored note repository to have SRP on this class and provide seperation of concern

--- app/src/main/java/com/asad/noteapp/features/note/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/asad/noteapp/features/note/data/repository/NoteRepositoryImpl.kt
@@ -1,30 +1,30 @@
 package com.asad.noteapp.features.note.data.repository
 
-import android.content.Context
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.workDataOf
 import com.asad.noteapp.features.note.data.dataSource.local.NoteLocalDataSource
 import com.asad.noteapp.core.domain.note.model.NoteModel
 import com.asad.noteapp.core.domain.note.model.toNoteEntity
 import com.asad.noteapp.core.domain.note.model.toNoteModel
-import com.asad.noteapp.core.service.notification.util.NotificationConstants
-import com.asad.noteapp.core.service.services.ReminderService
+import com.asad.noteapp.features.note.data.reminderManager.ReminderLauncher
 import com.asad.noteapp.features.note.domain.repository.NoteRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
-import java.util.Calendar
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class NoteRepositoryImpl @Inject constructor(
     private val noteLocalDataSource: NoteLocalDataSource,
-    @ApplicationContext private val context: Context,
+    private val reminderLauncher: ReminderLauncher
 ) : NoteRepository {
 
+    /**
+     * instead of sending request to reminder launcher,
+     * we can observe the note list from room database and set the reminder
+     * */
     override suspend fun insertNote(note: NoteModel) {
         noteLocalDataSource.insertNote(note = note.toNoteEntity())
         if (note.reminder != null)
-            setReminder(noteId = 0, noteTitle = note.title, noteBody = note.note)
+            reminderLauncher.setReminder(
+                noteId = 0,
+                noteTitle = note.title,
+                noteBody = note.note
+            )
     }
 
     override suspend fun updateNote(note: NoteModel) {
@@ -39,19 +39,4 @@ class NoteRepositoryImpl @Inject constructor(
         noteLocalDataSource
             .fetchNote(noteId = noteId)
             ?.toNoteModel()
-
-    fun setReminder(noteId: Int, noteTitle: String, noteBody: String?) {
-        val data = workDataOf(
-            NotificationConstants.ID_KEY to noteId,
-            NotificationConstants.TITLE_KEY to noteTitle,
-            NotificationConstants.NOTE_KEY to noteBody
-        )
-
-        val workRequest = OneTimeWorkRequestBuilder<ReminderService>()
-            .setInitialDelay(10, TimeUnit.SECONDS)
-            .setInputData(data)
-            .build()
-
-        WorkManager.getInstance(context).enqueue(workRequest)
-    }
 }
