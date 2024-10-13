83566e73e439434bca7ba9de7c8fbc5526bebf5a
asadkarami <asad.karammi@gmail.com>
2024-10-14 01:17:49 +0330

    chore: Added preview for home screen

--- app/src/main/java/com/asad/noteapp/features/home/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/asad/noteapp/features/home/presentation/screen/HomeScreen.kt
@@ -34,6 +34,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,6 +44,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -225,3 +228,16 @@ fun HomeContent(
     }
 }
 
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun HomeContentPreview() {
+    val homeUiState = remember { mutableStateOf(HomeUiState()) } //
+    HomeContent(
+        uiState = homeUiState,
+        windowSize = rememberWindowSize(),
+        onCreateNoteClicked = {},
+        onNoteClicked = {},
+        onLayoutChanged = {},
+        onMenuClicked = {}
+    )
+}
