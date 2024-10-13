8dc97b8a9412b79960a3e82f4a5845f5d699fd4b
asadkarami <asad.karammi@gmail.com>
2024-10-14 01:32:09 +0330

    chore: Refactored search bar compoent, Added preview for it

--- app/src/main/java/com/asad/noteapp/features/home/presentation/component/NoteToolbarComponent.kt
+++ app/src/main/java/com/asad/noteapp/features/home/presentation/component/NoteToolbarComponent.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
@@ -21,11 +25,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.asad.noteapp.R
@@ -68,16 +74,15 @@ fun RowScope.NoteToolbarComponent(
             horizontalArrangement = Arrangement.Center
         ) {
 
-            Icon(
-                painter = painterResource(R.drawable.ic_search_normal),
-                contentDescription = "search icon"
-            )
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            BasicTextField(
+            TextField(
                 value = textValue.value,
-                onValueChange = { textValue.value = it },
+                onValueChange = {
+                    if (textValue.value.length <= 15)
+                        textValue.value = it
+                },
                 maxLines = 1,
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -88,6 +93,21 @@ fun RowScope.NoteToolbarComponent(
                     lineHeight = 40.sp,
                     textAlign = TextAlign.Start,
                     textDecoration = TextDecoration.None
+                ),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_search_normal),
+                        contentDescription = "search icon"
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedTextColor = Color.Black,
+                    focusedTextColor = Color.Black,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    cursorColor = Color.Black
                 )
             )
         }
@@ -109,4 +129,20 @@ fun RowScope.NoteToolbarComponent(
         contentDescription = "menu"
     )
     Spacer(modifier = Modifier.width(8.dp))
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun NoteToolbarComponentPreview() {
+    Row(
+        modifier = Modifier.padding(24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        NoteToolbarComponent(
+            uiState = remember { mutableStateOf(HomeUiState()) },
+            onLayoutChanged = {},
+            onMenuClicked = {}
+        )
+    }
 }
